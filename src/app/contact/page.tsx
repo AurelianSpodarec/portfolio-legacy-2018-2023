846ffeaf054177841beba6ee543f26a86d1d83b6
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-18 23:52:47 +0100

    feat: Added new files for About and Contact as well as made them 4spaces deep

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+function Contact() {
+    return (
+        <>
+        <Head>
+            <title>Contact - Aurelian Spodarec</title>
+            <meta
+                name="description"
+                content="I’m Aurelian Spodarec. I live in Manchester, United Kingdom where I develop design and develop webapps."
+            />
+        </Head>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Contact
+        </main>
+        </>
+    )
+}
+
+export default Contact;
