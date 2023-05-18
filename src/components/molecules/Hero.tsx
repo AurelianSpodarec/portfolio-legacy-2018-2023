b565ca01b725db76140692b9dda9fdaa8556f6ee
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-19 00:30:14 +0100

    fix: Title and Description are now in a column, wheres before they were on one line

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -4,13 +4,13 @@ function Hero({ title, description, image }:HeroProps) {
     return (
         <div className="relative h-[500px]">
             <div className="absolute top-0 right-0 bottom-0 left-0 z-10 bg-black/60 h-full w-full"></div>
-            <Image src={image.url} alt={image.alt} className="w-full h-full object-cover" fill />
+            <Image src={image.url} alt={image.alt} fill className="w-full h-full object-cover" />
 
-            <div className="absolute z-20 h-full w-full">
-            <div className="flex justify-center items-center h-full w-full">
+            <div className="absolute z-20 h-full w-full px-8">
+            <div className="flex flex-col justify-center items-center h-full w-full">
 
-                <h1 className="text-white text-5xl font-bold">{title}</h1>
-                {description && <p className="text-gray-700 text-md">{description}</p>}
+                <h1 className="text-white text-5xl font-bold mb-2">{title}</h1>
+                {description && <p className="text-gray-300 text-md">{description}</p>}
 
             </div>
             </div>
