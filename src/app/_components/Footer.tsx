b3c78661ae4863ef709c2a9e515a7bef37a397d6
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-19 00:43:06 +0100

    feat: Created footer for desktop

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,39 @@
+import Link from "next/link";
+
 function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="bg-[#262626] py-6">
+        
+            
+            <div className="flex justify-between items-center">
+                <Link href="/" className="text-white text-3xl font-bold">Aurelian S</Link>
+
+                <div className="flex space-x-6 text-white">
+                    <a href="tel:0000">
+                        icon
+                        <span>Mobile</span>
+                    </a>
+                    <a href="mailto:aurelianxspodarec@gmail.com">
+                        icon
+                        <span>Email Me</span>
+                    </a>
+                </div>
+            </div>
+
+            <hr className="my-6" />
+
+            <div className="flex justify-between items-center">
+                <p className="text-white text-sm">&copy; Aurelian Spodarec {new Date().getFullYear()}. All rights reserved</p>
+
+                <nav className="text-white text-sm space-x-6">
+                    <a href="#">LinkedIn</a>
+                    <a href="#">FQA</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Github</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </div>
+
         </footer>
     )
 }
