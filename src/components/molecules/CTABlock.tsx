049e7bf32c11cd0b90de83294ad9654c18d4e586
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-05-19 01:35:44 +0100

    feat: Added basic CTA block component

---
+++ src/components/molecules/CTABlock.tsx
@@ -0,0 +1,29 @@
+import { IImage } from "@/app/interfaces/IImage";
+import { ILink } from "@/app/interfaces/ILink";
+import Image from "next/image";
+import Link from "next/link";
+
+function CTABlock({ title, image, link }:CTABlockProps) {
+    return (
+        <div className="relative overflow-hidden h-full group">
+
+            <div className="absolute top-0 right-0 bottom-0 left-0 z-10 bg-black/60 h-full w-full"></div>
+            <Image src={image.url} alt={image.alt} fill className="w-full h-full object-cover transition duration-100 ease-linear group-hover:scale-105" />
+
+            <div className="absolute z-20 h-full w-full px-8">
+            <div className="flex flex-col justify-center items-center h-full w-full">
+                <span className="text-white text-4xl font-bold uppercase">{title}</span>
+                <Link href={link.url}>View</Link>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default CTABlock;
+
+interface CTABlockProps {
+    title: string;
+    image: IImage;
+    link: ILink;
+}
