2924481729ab02b522afc3c0d1c7b0d9de87f83c
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-03-25 17:16:24 +0100

    update: Added some styling for contact box

--- src/app/_components/BoxContact.tsx
+++ src/app/_components/BoxContact.tsx
@@ -3,24 +3,24 @@ import { Input } from "@/components/atoms/Input";
 
 function BoxContact() {
   return (
-    <section>
-      <div>
+    <section className="bg-[#f5f5f5] py-24">
+      <div className="text-center">
         <h3>Fancy a chat?</h3>
         <p>Got a problem, idea or after some insight? Leave me a message and we'll be in touch</p>
       </div>
 
-      <div>
+      <div className="grid grid-cols-2 max-w-5xl mx-auto">
         <div>
           <Input name="name" type="text" placeholder="Name" />
           <Input name="email" type="email" placeholder="Email" />
           <Input name="telephone" type="tel"placeholder="Tel No." />
         </div>
         <div>
-          <textarea/>
+          <textarea className="w-full" />
         </div>
       </div>
 
-      <Button>SEND</Button>
+      <Button className="mx-auto text-center">SEND</Button>
     </section>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,85 +6,85 @@ import Image from "next/image";
 import BoxContact from "./_components/BoxContact";
 
 function Home() {
-    return (
-        <>
-            <Hero
-                title="Home"
-                image={{
-                    url: "/images/blur-cafe-chairs-min.jpg",
-                    alt: "Alrt"
-                }}
-            />
+  return (
+    <>
+      <Hero
+        title="Home"
+        image={{
+          url: "/images/blur-cafe-chairs-min.jpg",
+          alt: "Alrt"
+        }}
+      />
 
-            <Section>
-                <Container>
+      <Section>
+        <Container>
 
-                    <p className="text-xl">I help business owners just like you, establish a strong presence online by taking a strategic approach. A pretty website, responsive desing - all of that is default for any website. What you need is a strategy that'll help convert your customers into leads.</p>
+          <p className="text-xl">I help business owners just like you, establish a strong presence online by taking a strategic approach. A pretty website, responsive desing - all of that is default for any website. What you need is a strategy that'll help convert your customers into leads.</p>
 
-                </Container>
-            </Section>
+        </Container>
+      </Section>
 
-            <Section size="clean">
+      <Section size="clean">
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 h-[700px]">
-                    <CTABlock
-                        title="My Services"
-                        image={{
-                            url: "/images/woman-writing.jpg",
-                            alt: ""
-                        }}
-                        link={{
-                            url: "/services"
-                        }}
-                    />
-                    <CTABlock
-                        title="The Process"
-                        image={{
-                            url: "/images/process-finger.jpeg",
-                            alt: ""
-                        }}
-                        link={{
-                            url: "/process",
-                        }}
-                    />
-                    <CTABlock
-                        title="My Work"
-                        image={{
-                            url: "/images/man-writing-computer.jpeg",
-                            alt: ""
-                        }}
-                        link={{
-                            url: "/projects"
-                        }}
-                    />
-                </div>
-            </Section>
+        <div className="grid grid-cols-1 lg:grid-cols-3 h-[700px]">
+          <CTABlock
+            title="My Services"
+            image={{
+              url: "/images/woman-writing.jpg",
+              alt: ""
+            }}
+            link={{
+              url: "/services"
+            }}
+          />
+          <CTABlock
+            title="The Process"
+            image={{
+              url: "/images/process-finger.jpeg",
+              alt: ""
+            }}
+            link={{
+              url: "/process",
+            }}
+          />
+          <CTABlock
+            title="My Work"
+            image={{
+              url: "/images/man-writing-computer.jpeg",
+              alt: ""
+            }}
+            link={{
+              url: "/projects"
+            }}
+          />
+        </div>
+      </Section>
 
-            <section>
-                <Container>
+      <section className="bg-[#f5f5f5] py-12">
+        <Container>
 
-                    <div className="grid">
-                        <div>
-                            <h2>About me</h2>
-                            <span>Helping you build a site that works the first time</span>
+          <div className="grid grid-cols-2">
+            <div>
+              <h2>About me</h2>
+              <span>Helping you build a site that works the first time</span>
 
-                            <p>Let me tell you something. I don't have 10years of experience. What I do have is a process. It's a step-by-step guide that will help us get the result you want.</p>
-                        </div>
-                        <div>
-                            <Image src="/images/aurelian-photo.jpg" width="500" height="500" alt="Aurelian sitting in front of a computer" />
-                        </div>
-                    </div>
+              <p>Let me tell you something. I don't have 10years of experience. What I do have is a process. It's a step-by-step guide that will help us get the result you want.</p>
+            </div>
+            <div>
+              <Image src="/images/aurelian-photo.jpg" width="500" height="500" alt="Aurelian sitting in front of a computer" />
+            </div>
+          </div>
 
-                </Container>
-            </section>
+        </Container>
+      </section>
 
-            <section>
-                <span>Awesome. Creative. Developer.</span>
-            </section>
+      <section className="bg-[#272c31] text-white py-28 text-center text-5xl uppercase font-bold">
+        <span>Awesome. Creative. Developer.</span>
+      </section>
 
-            <BoxContact />
-        </>
-    )
+      <BoxContact />
+    </>
+  )
 }
 
 export default Home;
