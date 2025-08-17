1d1242772dca900d7fd905d5f00868d0414ecbe8
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-08-17 17:29:44 +0100

    Initial commit

---
+++ _archive/src/app/layout.tsx
@@ -0,0 +1,53 @@
+import Header from './_components/Header'
+import Footer from './_components/Footer'
+
+import './../styles/styles.scss'
+
+import { Montserrat, Lato, Roboto_Slab } from 'next/font/google'
+import { Providers } from './providers'
+
+const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat"
+})
+
+const lato = Lato({
+  weight: ['100', '300', '400', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-lato"
+})
+
+const roboto = Roboto_Slab({
+  weight: ['300', '400'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-roboto-slab"
+})
+
+export const metadata = {
+  title: 'Home = Aurelian Spodarec',
+  description: 'Aurelian Spodarec 2018 personal website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} ${lato.variable} ${roboto.variable}`}>
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,103 @@
 import Image from "next/image";
 
-import Container from "@/components/_layout/Container";
-import Section from "@/components/_layout/Section";
-
-import CTABlock from "@/components/molecules/CTABlock";
-import Hero from "@/components/molecules/Hero";
-import MainHero from "@/components/molecules/MainHero";
-
-import BoxContact from "./_components/BoxContact";
-
-function Home() {
+export default function Home() {
   return (
-    <>
-      <MainHero
-        title="Brochure Websites"
-        description="Make your website best salesperson"
-        image={{
-          url: "/images/blur-cafe-chairs-min.jpg",
-          alt: "Alrt"
-        }}
-      />
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-      <Section>
-        <Container>
-
-          <p className="text-xl">I help business owners just like you, establish a strong presence online by taking a strategic approach. A pretty website, responsive desing - all of that is default for any website. What you need is a strategy that'll help convert your customers into leads.</p>
-
-        </Container>
-      </Section>
-
-      <Section size="clean">
-
-        <div className="grid grid-cols-1 lg:grid-cols-3 h-[700px]">
-          <CTABlock
-            title="My Services"
-            image={{
-              url: "/images/woman-writing.jpg",
-              alt: ""
-            }}
-            link={{
-              url: "/services"
-            }}
-          />
-          <CTABlock
-            title="The Process"
-            image={{
-              url: "/images/process-finger.jpeg",
-              alt: ""
-            }}
-            link={{
-              url: "/process",
-            }}
-          />
-          <CTABlock
-            title="My Work"
-            image={{
-              url: "/images/man-writing-computer.jpeg",
-              alt: ""
-            }}
-            link={{
-              url: "/projects"
-            }}
-          />
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      </Section>
-
-      <Section>
-        <Container>
-
-          <div className="grid grid-cols-2">
-            <div>
-              <h2>About me</h2>
-              <span>Helping you build a site that works the first time</span>
-
-              <p>Let me tell you something. I don't have 10years of experience. What I do have is a process. It's a step-by-step guide that will help us get the result you want.</p>
-            </div>
-            <div>
-              <Image src="/images/aurelian-photo.jpg" width="500" height="500" alt="Aurelian sitting in front of a computer" />
-            </div>
-          </div>
-
-        </Container>
-      </Section>
-
-      <Section size="lg" className="bg-[#272c31] text-white text-center text-5xl uppercase font-bold">
-        <span>Awesome. Creative. Developer.</span>
-      </Section>
-
-      <BoxContact />
-    </>
-  )
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default Home;
