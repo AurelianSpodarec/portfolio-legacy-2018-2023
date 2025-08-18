7b3641d37e2d4290e00e3ea7a4afb841893aee57
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-08-18 15:39:16 +0100

    feat: add scrollbar, font family and basic theming variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,31 @@ import "./../styles/globals.css";
 import WebHeader from "./(pages)/_components/Header";
 import WebFooter from "./(pages)/_components/Footer";
 
+import { Montserrat, Noto_Serif_Georgian } from "next/font/google";
+
 export const metadata: Metadata = {
   title: "Next.js Dashboard with TailwindCSS",
   description: "Created with best practices in mind",
 };
 
+export const montserrat = Montserrat({
+  weight: ['400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat'
+})
+
+export const georgia = Noto_Serif_Georgian({
+  weight: ['400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-serif-georgian'
+})
+
 function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`antialiased scrollbar`}>
+      <body className={`antialiased scrollbar ${montserrat.variable} ${georgia.variable}`}>
         <WebHeader />
         {children}
         <WebFooter />
