72accea1cb3fd2652fa8f4d3caf3650a4e0f2ce4
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-08-20 11:39:21 +0100

    chore: improve styling

--- src/app/(pages)/(root)/_sections/Intro.tsx
+++ src/app/(pages)/(root)/_sections/Intro.tsx
@@ -3,10 +3,10 @@ import Section from "@/components/Section"
 
 function SectionIntro() {
   return (
-    <Section>
-      <Container>
-        <p className="text-xl">I help business owners just like you, establish a strong presence online by taking a strategic approach. A pretty website, responsive desing - all of that is default for any website. What you need is a strategy that'll help convert your customers into leads.</p>
-      </Container>
+    <Section id="#intro">
+      <div className="max-w-[1000px] font-primary mx-auto">
+        <p className="text-[#222] text-2xl leading-[2em]">I help business owners just like you, establish a strong presence online by taking a strategic approach. A pretty website, responsive desing - all of that is default for any website. What you need is a strategy that'll help convert your customers into leads.</p>
+      </div>
     </Section>
   )
 }
